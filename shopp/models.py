8a29dec43a35ce81ab9adbe418c6977f92108548
Azizbek20051905 <ailhomjonov1909@gmail.com>
2025-07-17 17:04:22 +0500

    Last commit

--- shopp/models.py
+++ shopp/models.py
@@ -11,5 +11,13 @@ class CustomeUser(AbstractUser):
     image = models.ImageField(upload_to="profile/")
     role = models.CharField(max_length=40, choices=ROLE_CHOICES, default="seller")
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 class Category(models.Model):
     name = models.CharField(max_length=250)
+    slug = AutoSlugField(populate_from="name")
+
+    def __str__(self):
+        return self.name
+
