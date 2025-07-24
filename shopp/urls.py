d5c2db6e2b74c066bcda62b54b2519acd35e9823
Azizbek20051905 <ailhomjonov1909@gmail.com>
2025-07-24 16:59:10 +0500

    update product commit

--- shopp/urls.py
+++ shopp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, productView, productsAdd
 
 urlpatterns = [
     path("", home, name="dashboard"),
+    path("products", productView, name="products"),
+    path("products/add/", productsAdd, name="products-add")
 ]
--- shopp/views.py
+++ shopp/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "index.html")
+
+def productView(request):
+    return render(request, 'product.html')
+
+def productsAdd(request):
+    return render(request, 'add_product.html')
+
