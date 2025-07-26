9f49eacc479a93baa88fc7220a76da4603dac311
Azizbek20051905 <ailhomjonov1909@gmail.com>
2025-07-26 17:16:23 +0500

    Add product info pages in DB

--- shopp/urls.py
+++ shopp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import home, productView, productsAdd
 
+app_name = "shopp"
+
 urlpatterns = [
     path("", home, name="dashboard"),
     path("products", productView, name="products"),
--- shopp/views.py
+++ shopp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Category, Product
 
 # Create your views here.
 def home(request):
@@ -8,5 +9,43 @@ def productView(request):
     return render(request, 'product.html')
 
 def productsAdd(request):
+    categories = Category.objects.all()
+    context = {
+        'categories':categories,
+    }
+
+    if request.method == "POST":
+        image = request.FILES.get('imageInput')
+        name = request.POST.get('NameInput')
+        category = request.POST['category']
+        category_name = request.POST['addCategory']
+        old_price = request.POST['OldPriceInput']
+        sell_price = request.POST['SellPriceInput']
+        amount = request.POST['AmountInput']
+        productType = request.POST['ProductType']
+        convertWhole = request.POST['ConvertWholeInput']
+        convertAmount = request.POST['ConvertAmountInput']
+        convertPrice = request.POST['ConvertPriceInput']
+
+        if not category:
+            if category_name:
+                category_add = Category.objects.create(name=category_name)
+                category_add.save()
+        else:
+            category_add = Category.objects.get(slug=category)
+        
+        product_add = Product.objects.create(name=name, category=category_add, 
+                                             old_price=old_price, 
+                                             sell_price=sell_price, 
+                                             type=productType,
+                                             amount=amount,
+                                             wholesale=convertWhole,
+                                             blockAmount=convertAmount,
+                                             blockPrice=convertPrice,
+                                             image=image)
+        product_add.save()
+
+        return redirect("shopp:products")
+
     return render(request, 'add_product.html')
 
