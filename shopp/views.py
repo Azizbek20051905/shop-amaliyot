6fc5436005442f58b2c7382efaf2527f714c06fd
Azizbek20051905 <ailhomjonov1909@gmail.com>
2025-07-30 21:47:07 +0500

    edit and update page

--- shopp/views.py
+++ shopp/views.py
@@ -22,6 +22,44 @@ def updateProductView(request, slug):
         'product': product,
         "categories": category
     }
+
+    if request.method == "POST":
+        image = request.FILES.get('imageInput')
+        name = request.POST.get('NameInput')
+        category = request.POST['category']
+        category_name = request.POST['addCategory']
+        old_price = request.POST['OldPriceInput']
+        sell_price = request.POST['SellPriceInput']
+        amount = request.POST['AmountInput']
+        productType = request.POST['ProductType']
+        convertWhole = request.POST['ConvertWholeInput']
+        convertAmount = request.POST['ConvertAmountInput']
+        convertPrice = request.POST['ConvertPriceInput']
+
+        if not category:
+            if category_name:
+                category_add = Category.objects.create(name=category_name)
+                category_add.save()
+        else:
+            category_add = Category.objects.get(slug=category)
+        
+        product.name = name
+        product.category = category_add
+        product.old_price = old_price
+        product.sell_price = sell_price
+        product.type = productType
+        product.amount = amount
+        product.wholesale = convertWhole
+        product.blockAmount = convertAmount
+        product.blockPrice = convertPrice
+        
+        if image:
+            product.image = image
+        
+        product.save()
+
+        return redirect("shopp:products")
+    
     return render(request, 'add_product.html', context)
 
 def productsAdd(request):
